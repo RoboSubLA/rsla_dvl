2b5c95971c6e16c0610c98723454ca0caf9db582
Bohdan Hrotovytskyy <bohdanhrotovytskyy@gmail.com>
2026-02-09 17:10:04 -0800

    feat: integrate ahrs messages to fix dvl heading

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <stdio.h>
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -12,12 +13,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
 
 #define BUFFER_SIZE 256
 #define PUBLISH_TIME_MS 1
 #define ELIMINATE_INF true
 #define DVL_IP "192.168.1.100"
-#define DVL_PORT 1034
+#define DVL_READ_PORT 1034
+#define DVL_WRITE_PORT 1033
 
 bool firstEnsemble = false;
 
@@ -27,10 +30,15 @@ class DvlPublisher : public rclcpp::Node
     DvlPublisher() : Node("dvl_publisher")
     {
       publisher_ = this->create_publisher<geometry_msgs::msg::Point>("/dvl/point", 10);
+      subscriber_ = this->create_subscription<geometry_msgs::msg::Quaternion>(
+            "/ahrs/quaternion",
+            10,
+            std::bind(&DvlPublisher::ahrs_callback, this, std::placeholders::_1));
       timer_ = this->create_wall_timer(
             std::chrono::milliseconds(PUBLISH_TIME_MS),
             std::bind(&DvlPublisher::timer_callback, this));
-      init_port();
+      init_ports();
+      init_dvl();
       init_decoder();
 
       RCLCPP_INFO(this->get_logger(), "Running DVL measurements");
@@ -42,35 +50,60 @@ class DvlPublisher : public rclcpp::Node
       decoder = nullptr;
       delete[] buffer;
       buffer = nullptr;
-      close(fd_);
+      close(fd_read_);
+      close(fd_write_);
     }
 
   private:
-    void init_port()
+    void init_ports()
     {
-      fd_ = socket(AF_INET, SOCK_STREAM, 0);
-      if (fd_ < 0) {
-          RCLCPP_ERROR(this->get_logger(), "Error creating socket");
-          return;
+      fd_read_ = socket(AF_INET, SOCK_STREAM, 0);
+      if (fd_read_ < 0) {
+        RCLCPP_ERROR(this->get_logger(), "Error creating socket (DVL read)");
+        return;
       }
 
       // Set up server address
       server_addr_.sin_family = AF_INET;
-      server_addr_.sin_port = htons(DVL_PORT);
+      server_addr_.sin_port = htons(DVL_READ_PORT);
       if (inet_pton(AF_INET, DVL_IP, &server_addr_.sin_addr) <= 0) {
-          RCLCPP_ERROR(this->get_logger(), "Invalid address/ Address not supported");
-          close(fd_);
-          return;
+        RCLCPP_ERROR(this->get_logger(), "Invalid address/ Address not supported (DVL read)");
+        close(fd_read_);
+        return;
       }
 
-      // Connect to DVL
-      if (connect(fd_, (struct sockaddr *)&server_addr_, sizeof(server_addr_)) < 0) {
-          RCLCPP_ERROR(this->get_logger(), "Connection failed");
-          close(fd_);
-          return;
+      // Connect to DVL read
+      if (connect(fd_read_, (struct sockaddr *)&server_addr_, sizeof(server_addr_)) < 0) {
+        RCLCPP_ERROR(this->get_logger(), "Connection failed (DVL read)");
+        close(fd_read_);
+        return;
       }
 
-      RCLCPP_INFO(this->get_logger(), "Connected to DVL at %s:%d", DVL_IP, DVL_PORT);
+      RCLCPP_INFO(this->get_logger(), "Connected to DVL Output at %s:%d", DVL_IP, DVL_READ_PORT);
+
+      fd_write_ = socket(AF_INET, SOCK_STREAM, 0);
+      if (fd_write_ < 0) {
+        RCLCPP_ERROR(this->get_logger(), "Error creating socket (DVL write)");
+        return;
+      }
+
+      // Set up server address
+      server_addr_.sin_family = AF_INET;
+      server_addr_.sin_port = htons(DVL_WRITE_PORT);
+      if (inet_pton(AF_INET, DVL_IP, &server_addr_.sin_addr) <= 0) {
+        RCLCPP_ERROR(this->get_logger(), "Invalid address/ Address not supported (DVL write)");
+        close(fd_write_);
+        return;
+      }
+
+      // Connect to DVL write
+      if (connect(fd_write_, (struct sockaddr *)&server_addr_, sizeof(server_addr_)) < 0) {
+        RCLCPP_ERROR(this->get_logger(), "Connection failed (DVL write)");
+        close(fd_write_);
+        return;
+      }
+
+      RCLCPP_INFO(this->get_logger(), "Connected to DVL Command at %s:%d", DVL_IP, DVL_WRITE_PORT);
     }
 
     void init_decoder()
@@ -79,10 +112,19 @@ class DvlPublisher : public rclcpp::Node
       tdym::PDD_InitializeDecoder(decoder); //init decoder
     }
 
+    void init_dvl()
+    {
+      dprintf(fd_write_, "===\r");
+      dprintf(fd_write_, "CR1\r");
+      dprintf(fd_write_, "EA+04500\r");
+      dprintf(fd_write_, "BJ100100000\r");
+      dprintf(fd_write_, "CS\r");
+    }
+
     void timer_callback()
     {
       std::memset(buffer, 0, BUFFER_SIZE);
-      int data = read(fd_, buffer, sizeof(buffer));
+      int data = read(fd_read_, buffer, sizeof(buffer));
 
       if (data > 0) {
         tdym::PDD_AddDecoderData(decoder, buffer, (int)data);
@@ -115,13 +157,38 @@ class DvlPublisher : public rclcpp::Node
           }
         } while (found > 0);
       }
-      
+    }
+
+    void ahrs_callback(const geometry_msgs::msg::Quaternion::SharedPtr msg){
+      // Conversion to Euler Angles from Quaternion
+      // roll (x-axis rotation)
+      double sinr_cosp = 2 * (msg->w * msg->x + msg->y * msg->z);
+      double cosr_cosp = 1 - 2 * (msg->x * msg->x + msg->y * msg->y);
+      double roll = atan2(sinr_cosp, cosr_cosp);
+
+      // pitch (y-axis rotation)
+      double sinp = 2 * (msg->w * msg->y - msg->z * msg->x);
+      double pitch = 0;
+      if (abs(sinp) >= 1)
+        pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+      else
+        pitch = asin(sinp);
+
+      // yaw (z-axis rotation)
+      double siny_cosp = 2 * (msg->w * msg->z + msg->x * msg->y);
+      double cosy_cosp = 1 - 2 * (msg->y * msg->y + msg->z * msg->z);
+      double yaw = atan2(siny_cosp, cosy_cosp);
+
+      dprintf(fd_write_, "EH%d,1\r", (int)(yaw * 18000 / M_PI));
+      dprintf(fd_write_, "EP%d,%d,1\r", (int)(pitch * 18000 / M_PI), (int)(roll * 18000 / M_PI));
     }
 
   //Variables
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr publisher_;
-  int fd_;
+  rclcpp::Subscription<geometry_msgs::msg::Quaternion>::SharedPtr subscriber_;
+  int fd_read_;
+  int fd_write_;
   struct sockaddr_in server_addr_;
   unsigned char* buffer = new unsigned char[BUFFER_SIZE];
   tdym::PDD_Decoder* decoder = new tdym::PDD_Decoder();
